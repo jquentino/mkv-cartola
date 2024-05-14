5b79d4f1c6880c1cdcd1d5e6510d9c1995578cc5
Jucelio Quentino <vitorquentino@gmail.com>
2024-05-14 03:42:26 -0300

    ENH: Add markov chain analisys for attackers

--- utils/data_acquisition.py
+++ utils/data_acquisition.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from cartolafc import Api
+from cartolafc.constants import MERCADO_FECHADO
+
 
 def get_players() -> pd.DataFrame:
     """Get players available in the market"""
@@ -10,9 +12,58 @@ def get_players() -> pd.DataFrame:
         player_record = {
             "nickname": player.apelido,
             "id": player.id,
-            "position": player.posicao.abreviacao
+            "position": player.posicao.abreviacao,
+            "price": player.preco,
+            "team": player.clube.abreviacao
         }
         records.append(player_record)
+    return pd.DataFrame.from_records(records)
+
+
+def get_matches() -> pd.DataFrame:
+    """Get all matches until the current round"""
+
+    api = Api()
+    max_rounds = api.mercado().rodada_atual 
+
+    records = []
+    for round in range(1, max_rounds):
+        matches = api.partidas(round)
+        for match in matches:
+            match_record = {
+                "date": match.data,
+                "home_team": match.clube_casa.abreviacao,
+                "away_team": match.clube_visitante.abreviacao,
+                "home_score": match.placar_casa,
+                "away_score": match.placar_visitante,
+                "round": round
+            }
+            records.append(match_record)
+    
+    return pd.DataFrame.from_records(records)
+
+
+def get_players_perfomance() -> pd.DataFrame:
+    """Get players perfomance information along the rounds"""
+
+    api = Api()
+    max_rounds = api.mercado().rodada_atual 
+
+    records = []
+    for round in range(1, max_rounds):
+        players_round = list(api.resultados_atletas(round).values())
+        for player in players_round:
+            record = {
+                "nickname": player.apelido,
+                "id": player.id,
+                "position": player.posicao.abreviacao,
+                "price": player.preco,
+                "team": player.clube.abreviacao,
+                "points": player.pontos,
+                "round": round
+            }
+            records.append(record)
+    
     return pd.DataFrame.from_records(records)
     
 
